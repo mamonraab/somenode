a9e127dec515fe3eb8dcfc3f092cd06055c5a406
mamon rasool <mamon@mamonrasool.com>
2016-10-08 18:58:21 +0300

    initilazie repo

---
+++ index.js
@@ -0,0 +1,53 @@
+// include the moudle
+var express = require('express');
+var app = express();
+var port = 3000;
+//midleware
+var middleware = {
+    requireAuthentication: function(req , res , next){
+ console.log('private area ');
+ next();
+    },
+    loger: function(req , res , next){
+console.log(req.method + '   ' + req.originalUrlm  + '   ' + new Date().toString());
+next();
+    }
+};
+// when we use app.use  that mean we use aaplication level middelware
+//that work in all the app route
+app.use(middleware.requireAuthentication);
+
+/*
+to use the  middelware only for spicific route
+
+*/
+
+app.get('/me',middleware.loger, function (req , res ){
+
+res.send(" <input type='text' name='mamon' value='axs' />   ");
+
+});
+
+// starting the http
+app.get('/', function (req , res){
+res.send('hello mamon node js is here new love !!!!!');
+});
+
+
+// making another page
+app.get('/about', function (req , res ){
+
+res.send(" <input type='text' name='mamon' value='axs' />   ");
+
+});
+
+// adding floder and its files to be brwse abel
+//you can visit these typing localhost:3000/index.html
+app.use(express.static(__dirname + '/www/v2'))
+
+
+//port to listin in 
+
+app.listen(port,function(){
+    console.log('mamon express started in port ' + port +' !!!');
+});
